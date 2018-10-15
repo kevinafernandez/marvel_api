55248a452e5fe95387a13c61614f72249ff51bb9
Kevin Fernandez <kevinafernandezm@gmail.com>
2018-10-14 23:11:58 -0300

    Vistas: Se mejoran vistas | Se crea CRUD para personas.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module MarvelApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.i18n.available_locales = ['es-CL', :es]
+    config.i18n.default_locale = :es
+    config.time_zone = 'America/Santiago'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -11,6 +11,9 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+ ActiveSupport::Inflector.inflections(:en) do |inflect|
+    inflect.irregular 'persona', 'personas'
+    inflect.irregular 'Persona', 'Personas'
+    inflect.irregular 'superheroe', 'superheroes'
+    inflect.irregular 'Superheroe', 'Superheroes'
+end
